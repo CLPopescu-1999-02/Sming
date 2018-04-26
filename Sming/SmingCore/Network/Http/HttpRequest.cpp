3f385ec037362e723514970f0995f3dde6b8bb73
Slavey Karadzhov <slav@attachix.com>
2018-04-26 14:23:11 +0200

    Removed unnecessary use of min.

--- Sming/SmingCore/Network/Http/HttpRequest.cpp
+++ Sming/SmingCore/Network/Http/HttpRequest.cpp
@@ -12,8 +12,6 @@
 
 #include "HttpRequest.h"
 
-#include <algorithm>
-
 HttpRequest::HttpRequest(const URL& uri) {
 	this->uri = uri;
 }
@@ -161,7 +159,7 @@ String HttpRequest::getBody()
 		char buf[1024];
 		while(stream->available() > 0) {
 			int available = memory->readMemoryBlock(buf, 1024);
-			memory->seek(std::max(available, 0));
+			memory->seek(available);
 			ret += String(buf, available);
 			if(available < 1024) {
 				break;
